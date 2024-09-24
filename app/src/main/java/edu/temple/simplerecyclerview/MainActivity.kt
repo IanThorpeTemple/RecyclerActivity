4db98fcd8b4753b36a210d6a25169a9aa8840e7b
Ian Thorpe <tup56195@temple.edu>
2024-09-24 15:18:26 -0400

    Finished implementing Recycler View

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,19 +2,21 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    // lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO Step 1: Reference RecyclerView object
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
-        //TODO Step 2: Provide a LayoutManager
-
-
-        //TODO Step 4: Provide a RecyclerView.Adapter
+        val numbers = IntArray(100){it + 1}
+        recyclerView.adapter = NumberDisplayAdapter(numbers)
     }
 }
